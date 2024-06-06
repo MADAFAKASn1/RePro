1afd2f06177aa72c5e392fbdb5bef4ac970bb4e3
MADAFAKASn1 <marcosandresmoldovanclase@gmail.com>
2024-06-07 00:37:43 +0200

    musica desde firebase funcionando

--- app/src/main/java/com/example/repro/ui/adaptadores/AdaptadorPersonalizado.java
+++ app/src/main/java/com/example/repro/ui/adaptadores/AdaptadorPersonalizado.java
@@ -1,62 +1,69 @@
 package com.example.repro.ui.adaptadores;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.repro.R;
+import com.google.firebase.storage.StorageReference;
 
-public class AdaptadorPersonalizado extends ArrayAdapter<String> {
-    private LayoutInflater inflater;
-    public AdaptadorPersonalizado(Context ctx, int txtViewResourceId, String[] objects){
-        super(ctx, txtViewResourceId, objects);
-        inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
-    @Override
-    public View getDropDownView(int position, View cnvtView, ViewGroup prnt){
-        return crearFilaPersonalizada(position, cnvtView, prnt);
-    }
-    @Override
-    public View getView(int pos, View cnvtView, ViewGroup prnt){
-        return crearFilaPersonalizada(pos, cnvtView, prnt);
-    }
-    public View crearFilaPersonalizada(int position, View convertView, ViewGroup parent){
-        String[] heroes = { "Batman","Capitan América",
-                "Iron man","Spiderman","Black Panther" };
+import java.io.IOException;
+import java.util.List;
 
-
-        View miFila = inflater.inflate(R.layout.adaptador_personalizado_inicio, parent, false);
-        /*TextView nombre = (TextView) miFila.findViewById(R.id.textoHeroes);
-        nombre.setText(heroes[position]);
-        ImageView imagen = (ImageView) miFila.findViewById(R.id.imagen);
-        imagen.setImageResource(imagenes[position]);*/
-        return miFila;
-    }
-}
-/*
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-
+public class AdaptadorPersonalizado extends RecyclerView.Adapter<AdaptadorPersonalizado.MyViewHolder> {
     private List<StorageReference> mData;
+    private MediaPlayer mediaPlayer;
+    private int currentPlayingPosition = -1;
+    private MyViewHolder currentPlayingHolder = null;
+    private Context context;
+    private LayoutInflater inflater;
 
-    public MyAdapter(List<StorageReference> data) {
+    public AdaptadorPersonalizado(List<StorageReference> data, Context ctx) {
+        super();
         this.mData = data;
+        this.context = ctx;
+        this.mediaPlayer = new MediaPlayer();
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        View view = inflater.inflate(R.layout.adaptador_personalizado_inicio, parent, false);
         return new MyViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
         StorageReference item = mData.get(position);
-        // Here you can set the data to your views in the item layout
+        holder.textViewName.setText(item.getName());
+
+        holder.playPauseButton.setImageResource(
+                holder.getAdapterPosition() == currentPlayingPosition ?
+                        R.drawable.icons8_pausa_30 : R.drawable.icons8_play_30
+        );
+
+        holder.playPauseButton.setOnClickListener(v -> {
+            if (currentPlayingPosition == holder.getAdapterPosition()) {
+                if (mediaPlayer.isPlaying()) {
+                    pauseAudio();
+                    currentPlayingHolder.textViewName.setSelected(false);
+                } else {
+                    resumeAudio();
+                    currentPlayingHolder.textViewName.setSelected(true);
+                }
+            } else {
+                playAudio(holder, item);
+            }
+        });
     }
 
     @Override
@@ -64,12 +71,62 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         return mData.size();
     }
 
+    private void playAudio(MyViewHolder holder, StorageReference item) {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            if (currentPlayingHolder != null) {
+                currentPlayingHolder.playPauseButton.setImageResource(R.drawable.icons8_play_30);
+                currentPlayingHolder.textViewName.setSelected(false); // Stop marquee effect on previous song's TextView
+            }
+        }
+        mediaPlayer.reset();
+
+        currentPlayingPosition = holder.getAdapterPosition();
+        currentPlayingHolder = holder;
+
+        item.getDownloadUrl().addOnSuccessListener(uri -> {
+            try {
+                mediaPlayer.setDataSource(uri.toString());
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+                holder.playPauseButton.setImageResource(R.drawable.icons8_pausa_30);
+                holder.textViewName.setSelected(true); // Start marquee effect on current song's TextView
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(mp -> {
+            holder.playPauseButton.setImageResource(R.drawable.icons8_play_30);
+            currentPlayingPosition = -1;
+            currentPlayingHolder = null;
+        });
+    }
+
+    private void pauseAudio() {
+        mediaPlayer.pause();
+        if (currentPlayingHolder != null) {
+            currentPlayingHolder.playPauseButton.setImageResource(R.drawable.icons8_play_30);
+        }
+    }
+
+    private void resumeAudio() {
+        mediaPlayer.start();
+        if (currentPlayingHolder != null) {
+            currentPlayingHolder.playPauseButton.setImageResource(R.drawable.icons8_pausa_30);
+        }
+    }
+
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // Here you can initialize your views in the item layout
+        TextView textViewName;
+        ImageView imageView;
+        ImageButton playPauseButton;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            // Here you can find your views in the item layout
+            textViewName = itemView.findViewById(R.id.textViewName);
+            imageView = itemView.findViewById(R.id.imageViewFondo);
+            playPauseButton = itemView.findViewById(R.id.play_pause);
         }
     }
-}*/
+}
--- app/src/main/java/com/example/repro/ui/inicio/InicioFragment.java
+++ app/src/main/java/com/example/repro/ui/inicio/InicioFragment.java
@@ -7,18 +7,26 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.repro.R;
 import com.example.repro.databinding.FragmentInicioBinding;
+import com.example.repro.ui.adaptadores.AdaptadorPersonalizado;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InicioFragment extends Fragment {
 
@@ -26,91 +34,106 @@ public class InicioFragment extends Fragment {
     private RecyclerView recyclerView;
     private Spinner spinner;
     private StorageReference mStorageRef;
-
+    private FirebaseAuth firebaseAuth;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentInicioBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
         mStorageRef = FirebaseStorage.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
 
         recyclerView = binding.recyclerViewInicio;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));  // Añadir LayoutManager
+
         spinner = binding.spinner;
 
-        // Populate the Spinner
+        //Spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
                 R.array.spinner_options, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-        // Set the listener
+        //Listener
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 switch (position) {
                     case 0: // Canciones
-                        //loadCanciones();
+                        loadCanciones();
                         break;
                     case 1: // Videos
-                        //loadVideos();
+                        loadVideos();
                         break;
                 }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Do nothing
-            }
+            public void onNothingSelected(AdapterView<?> parent) {}
         });
 
         return root;
     }
 
-    /*private void loadCanciones() {
-        StorageReference cancionesRef = mStorageRef.child("canciones");
-
-        cancionesRef.listAll()
-                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
-                    @Override
-                    public void onSuccess(ListResult listResult) {
-                        List<StorageReference> data = new ArrayList<>();
-                        for (StorageReference fileRef : listResult.getItems()) {
-                            data.add(fileRef);
+    private void loadCanciones() {
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        System.out.println("currentUser " + currentUser);
+        if (currentUser != null) {
+            StorageReference cancionesRef = mStorageRef.child("canciones");
+            System.out.println("cancionesRef " + cancionesRef);
+            cancionesRef.listAll()
+                    .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                        @Override
+                        public void onSuccess(ListResult listResult) {
+                            System.out.println("cancionesRef.listAll() " + cancionesRef.listAll());
+                            List<StorageReference> data = new ArrayList<>();
+                            for (StorageReference fileRef : listResult.getItems()) {
+                                System.out.println("fileRef " + fileRef);
+                                data.add(fileRef);
+                            }
+                            AdaptadorPersonalizado adapter = new AdaptadorPersonalizado(data, getContext());
+                            recyclerView.setAdapter(adapter);
                         }
-                        MyAdapter adapter = new MyAdapter(data);
-                        recyclerView.setAdapter(adapter);
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Handle any errors
-                    }
-                });
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            System.out.println("Error loadCanciones " + exception.getMessage());
+                        }
+                    });
+        } else {
+            Toast.makeText(getContext(), "Usuario no autenticado", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void loadVideos() {
-        StorageReference videosRef = mStorageRef.child("videos");
-
-        videosRef.listAll()
-                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
-                    @Override
-                    public void onSuccess(ListResult listResult) {
-                        List<StorageReference> data = new ArrayList<>();
-                        for (StorageReference fileRef : listResult.getItems()) {
-                            data.add(fileRef);
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if (currentUser != null) {
+            StorageReference videosRef = mStorageRef.child("videos");
+            videosRef.listAll()
+                    .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                        @Override
+                        public void onSuccess(ListResult listResult) {
+                            List<StorageReference> data = new ArrayList<>();
+                            for (StorageReference fileRef : listResult.getItems()) {
+                                data.add(fileRef);
+                            }
+                            AdaptadorPersonalizado adapter = new AdaptadorPersonalizado(data, getContext());
+                            recyclerView.setAdapter(adapter);
                         }
-                        MyAdapter adapter = new MyAdapter(data);
-                        recyclerView.setAdapter(adapter);
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Handle any errors
-                    }
-                });
-    }*/
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            System.out.println("Error loadVideos " + exception.getMessage());
+                        }
+                    });
+        } else {
+            Toast.makeText(getContext(), "Usuario no autenticado", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     public void onDestroyView() {
